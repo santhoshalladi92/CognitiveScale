4c917d4b92d99022e5182fc9a5d4b3a406f7a786
rohit <t.e.s.rohit@gmail.com>
2014-09-19 15:18:37 +0530

    1.0

--- Day1.py
+++ Day1.py
@@ -1,7 +1,15 @@
 __author__ = 'santhosh'
-class parent:
+class Day:
     def __init__(self):
         print "date: Day is 19"
+
+class Month(Day):
+    def __init__(self):
+        print "Month is September"
+
+class Year(Month):
+    def __init__(self):
+        print "Year is 2014"
 
 p1 = parent()
 
